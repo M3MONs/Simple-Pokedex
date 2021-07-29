6b5a3dfaa7ed787173ed470f0828a815867b9791
M3MONs <alan.skrrrz@gmail.com>
2021-07-29 13:05:01 +0200

    pagination and displaying in order

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ let next, prev;
 // GET LIST IN HTML FILE
 const pokemonList = document.getElementById("pokemonList");
 
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+// prevBtn.addEventListener("click", );
+
+function loadAgain(newUrl) {
+   pokemonList.innerHTML = "";
+   url = newUrl;
+   fetchMultiplePokemons();
+}
+
 fetchMultiplePokemons();
 
 function fetchMultiplePokemons() {
@@ -13,24 +24,40 @@ function fetchMultiplePokemons() {
    })
       .then((res) => res.json())
       .then((res) => {
-         res.results.forEach((pokemon) => {
-            fetchSinglePokemon(pokemon.url);
+         next = res.next;
+         prev = res.prev;
+
+         return Promise.all(
+            res.results.map((poke) => {
+               return fetch(poke.url).then((res) => res.json());
+            })
+         );
+      })
+      .then((res) => {
+         res.forEach((pokemon) => {
+            pokemonList.innerHTML += `
+         <li class="${pokemon.types[0].type.name}">
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            <h1>${pokemon.name}</h1>
+            <h3>Height: ${pokemon.height}</h3>
+            <h3>Weight: ${pokemon.weight}</h3>
+         </li>`;
          });
       });
 }
 
-function fetchSinglePokemon(pokemonUrl) {
-   fetch(pokemonUrl, {
-      method: "GET",
-   })
-      .then((res) => res.json())
-      .then((res) => {
-         pokemonList.innerHTML += `
-         <li class="${res.types[0].type.name}">
-            <img src="${res.sprites.other.dream_world.front_default}" alt="${res.name}">
-            <h1>${res.name}</h1>
-            <h3>Height: ${res.height}</h3>
-            <h3>Weight: ${res.weight}</h3>
-         </li>`;
-      });
-}
+nextBtn.addEventListener("click", function () {
+   if (typeof next !== "undefined") {
+      pokemonList.innerHTML = "";
+      url = next;
+      fetchMultiplePokemons();
+   }
+});
+
+prevBtn.addEventListener("click", function () {
+   if (typeof prev !== "undefined") {
+      pokemonList.innerHTML = "";
+      url = prev;
+      fetchMultiplePokemons();
+   }
+});
